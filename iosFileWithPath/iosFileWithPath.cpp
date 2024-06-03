32dbfe1a71709f2d7f383a2c9c7562f1f3b06a19
KamiliaNurmala <kamilia.nurmala.ft23@mail.umy.ac.id>
2024-06-03 10:09:36 +0700

    membuka file dalam mode nulis

--- iosFileWithPath/iosFileWithPath.cpp
+++ iosFileWithPath/iosFileWithPath.cpp
@@ -2,3 +2,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+	string baris;
+	string namaFile;
+
+	cout << "Masukan Nama File : ";
+	cin >> namaFile;
+
+	// membuka file dalam mode nulis
+	ofstream outfile;
+}
